eb24e031e4f65c92615c63678c09d0deeca8b347
W1ll1am31 <william.cresswell5@gmail.com>
2026-05-18 00:45:47 +0100

    fix: remove old code

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { AuthUIProvider } from "@daveyplate/better-auth-ui";
-import { ShimmerProvider } from "@shimmer-from-structure/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -13,26 +12,20 @@ export function Providers({ children }: { children: ReactNode }) {
   const queryClient = new QueryClient();
 
   return (
-    <ShimmerProvider
-      config={{
-        shimmerColor: "oklch(50.563% 0.12078 49.268)",
-      }}
-    >
-      <QueryClientProvider client={queryClient}>
-        <AuthUIProvider
-          authClient={authClient}
-          navigate={router.push}
-          replace={router.replace}
-          onSessionChange={() => {
-            // Clear router cache (protected routes)
-            router.refresh();
-          }}
-          Link={Link}
-          social={{ providers: ["google"] }}
-        >
-          {children}
-        </AuthUIProvider>
-      </QueryClientProvider>
-    </ShimmerProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthUIProvider
+        authClient={authClient}
+        navigate={router.push}
+        replace={router.replace}
+        onSessionChange={() => {
+          // Clear router cache (protected routes)
+          router.refresh();
+        }}
+        Link={Link}
+        social={{ providers: ["google"] }}
+      >
+        {children}
+      </AuthUIProvider>
+    </QueryClientProvider>
   );
 }
